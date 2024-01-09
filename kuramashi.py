1abd6bf5a925e2ef00e07e37cb3c7e60d9fdad8e
eg2525 <149653970+eg2525@users.noreply.github.com>
2024-01-09 11:11:15 +0900

    Add files via upload

--- kuramashi.py
+++ kuramashi.py
@@ -6,8 +6,8 @@ def main():
 
     # Excelファイルのアップロード部分
     st.subheader('二つのExcelファイルをアップロードしてください')
-    file1 = st.file_uploader('一つ目のExcelファイルをアップロード', type=['xlsx'])
-    file2 = st.file_uploader('二つ目のExcelファイルをアップロード', type=['xlsx'])
+    file1 = st.file_uploader('一つ目のExcelファイルをアップロード', type=['xlsx'], key="file1")
+    file2 = st.file_uploader('二つ目のExcelファイルをアップロード', type=['xlsx'], key="file2")
 
     # 両方のファイルがアップロードされた場合の処理
     if file1 and file2:
@@ -17,29 +17,21 @@ def main():
 
         # シートを選択するためのプルダウンメニュー
         st.subheader('各ファイルからシートを選択してください')
-        selected_sheet1 = st.selectbox('一つ目のファイルからシートを選択', sheets_file1)
-        selected_sheet2 = st.selectbox('二つ目のファイルからシートを選択', sheets_file2)
+        selected_sheet1 = st.selectbox('一つ目のファイルからシートを選択', sheets_file1, key="selected_sheet1")
+        selected_sheet2 = st.selectbox('二つ目のファイルからシートを選択', sheets_file2, key="selected_sheet2")
 
-        # 情報転記を実行するボタン
-        if st.button('情報転記を実行'):
-            st.write('情報転記を開始します...')
+        # シート選択後の処理
+        if st.button('シート選択完了'):
+            # 選択されたシートをセッションステートに保存
+            st.session_state["df1"] = pd.read_excel(file1, sheet_name=selected_sheet1, header=5)  # 6行目がヘッダー
+            st.session_state["df2"] = pd.read_excel(file2, sheet_name=selected_sheet2, header=0)  # 1行目がヘッダー
 
-            # 選択されたシートを読み込み（ヘッダー行を指定）
-            df1 = pd.read_excel(file1, sheet_name=selected_sheet1, header=5)  # 6行目がヘッダー
-            df2 = pd.read_excel(file2, sheet_name=selected_sheet2, header=0)  # 1行目がヘッダー
-
-            # 2のシートのヘッダー名を表示し、1のシートのヘッダーから選択できるようにする
-            st.subheader('2のシートのヘッダー名に対応する1のシートのヘッダーを選択してください')
-            header_mappings = {}
-            for col in df2.columns:
-                options = list(df1.columns)
-                selected_option = st.selectbox(f'2のシートのヘッダー "{col}" に対応する1のシートのヘッダーを選択:', options, key=col)
-                header_mappings[col] = selected_option
-
-            st.write('選択が完了しました。')
-            # ここに転記のロジックを追加
-
-            st.write('情報転記が完了しました！')
+    # ヘッダーのマッピング処理
+    if "df1" in st.session_state and "df2" in st.session_state:
+        st.subheader('2のシートのヘッダー名に対応する1のシートのヘッダーを選択してください')
+        for col in st.session_state["df2"].columns:
+            options = list(st.session_state["df1"].columns)
+            st.session_state[col] = st.selectbox(f'2のシートのヘッダー "{col}" に対応する1のシートのヘッダーを選択:', options, key=col)
 
 # アプリを実行
 if __name__ == "__main__":
