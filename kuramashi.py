a1f3efdb433dcd2b29f10d4d161da43924b07815
eg2525 <149653970+eg2525@users.noreply.github.com>
2024-01-09 11:26:51 +0900

    Add files via upload

--- kuramashi.py
+++ kuramashi.py
@@ -11,16 +11,13 @@ def main():
 
     # ファイルがアップロードされた場合にシートの選択
     if file1 and file2:
-        # アップロードされたファイルからシート名を取得
         sheets_file1 = pd.ExcelFile(file1).sheet_names
         sheets_file2 = pd.ExcelFile(file2).sheet_names
 
-        # シートを選択するためのプルダウンメニュー
         st.subheader('各ファイルからシートを選択してください')
         selected_sheet1 = st.selectbox('一つ目のファイルからシートを選択', sheets_file1, key="selected_sheet1")
         selected_sheet2 = st.selectbox('二つ目のファイルからシートを選択', sheets_file2, key="selected_sheet2")
 
-        # シート選択完了ボタン
         if st.button('シート選択完了'):
             st.session_state['selected_sheet1'] = selected_sheet1
             st.session_state['selected_sheet2'] = selected_sheet2
@@ -29,11 +26,9 @@ def main():
 
     # シートが選択されている場合の処理
     if 'selected_sheet1' in st.session_state and 'selected_sheet2' in st.session_state:
-        # 選択されたシートを読み込み（ヘッダー行を指定）
-        df1 = pd.read_excel(st.session_state['file1'], sheet_name=st.session_state['selected_sheet1'], header=5)  # 6行目がヘッダー
-        df2 = pd.read_excel(st.session_state['file2'], sheet_name=st.session_state['selected_sheet2'], header=0)  # 1行目がヘッダー
+        df1 = pd.read_excel(st.session_state['file1'], sheet_name=st.session_state['selected_sheet1'], header=5)
+        df2 = pd.read_excel(st.session_state['file2'], sheet_name=st.session_state['selected_sheet2'], header=0)
 
-        # 2のシートのヘッダー名に対応する1のシートのヘッダーを選択
         st.subheader('2のシートのヘッダー名に対応する1のシートのヘッダーを選択してください')
         header_mappings = {}
         for col in df2.columns:
@@ -41,11 +36,13 @@ def main():
             selected_option = st.selectbox(f'2のシートのヘッダー "{col}" に対応する1のシートのヘッダーを選択:', options, key=col+"_header")
             header_mappings[col] = selected_option
 
-        st.write('選択が完了しました。')
-        # ここに転記のロジックを追加
+        # 転記実行ボタン
+        if st.button('転記実行'):
+            # ここに転記のロジックを追加
+            st.write('情報転記を実行中...')
 
-        st.write('情報転記が完了しました！')
+            # 転記処理が完了したら表示
+            st.write('情報転記が完了しました！')
 
-# アプリを実行
 if __name__ == "__main__":
     main()
