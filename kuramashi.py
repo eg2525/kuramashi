3c05525a48e50dc8f4e2384d5bf521621d81dd9e
eg2525 <149653970+eg2525@users.noreply.github.com>
2024-01-09 12:48:23 +0900

    Add files via upload

--- kuramashi.py
+++ kuramashi.py
@@ -18,17 +18,11 @@ def main():
         selected_sheet1 = st.selectbox('一つ目のファイルからシートを選択', sheets_file1, key="selected_sheet1")
         selected_sheet2 = st.selectbox('二つ目のファイルからシートを選択', sheets_file2, key="selected_sheet2")
 
-        if st.button('シート選択完了'):
-            st.session_state['selected_sheet1'] = selected_sheet1
-            st.session_state['selected_sheet2'] = selected_sheet2
-            st.session_state['file1'] = file1
-            st.session_state['file2'] = file2
+        # 選択されたシートを読み込み（ヘッダー行を指定）
+        df1 = pd.read_excel(file1, sheet_name=selected_sheet1, header=5)
+        df2 = pd.read_excel(file2, sheet_name=selected_sheet2, header=0)
 
-    # シートが選択されている場合の処理
-    if 'selected_sheet1' in st.session_state and 'selected_sheet2' in st.session_state:
-        df1 = pd.read_excel(st.session_state['file1'], sheet_name=st.session_state['selected_sheet1'], header=5)
-        df2 = pd.read_excel(st.session_state['file2'], sheet_name=st.session_state['selected_sheet2'], header=0)
-
+        # 2のシートのヘッダー名に対応する1のシートのヘッダーを選択
         st.subheader('2のシートのヘッダー名に対応する1のシートのヘッダーを選択してください')
         header_mappings = {}
         for col in df2.columns:
