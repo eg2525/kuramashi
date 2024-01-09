e0268b4c2337c4732eedf1d89402b522cc1d2c16
eg2525 <149653970+eg2525@users.noreply.github.com>
2024-01-09 13:08:20 +0900

    Add files via upload

--- kuramashi.py
+++ kuramashi.py
@@ -9,7 +9,6 @@ def main():
     file1 = st.file_uploader('一つ目のExcelファイルをアップロード', type=['xlsx'], key="file1")
     file2 = st.file_uploader('二つ目のExcelファイルをアップロード', type=['xlsx'], key="file2")
 
-    # ファイルがアップロードされた場合にシートの選択
     if file1 and file2:
         sheets_file1 = pd.ExcelFile(file1).sheet_names
         sheets_file2 = pd.ExcelFile(file2).sheet_names
@@ -18,24 +17,31 @@ def main():
         selected_sheet1 = st.selectbox('一つ目のファイルからシートを選択', sheets_file1, key="selected_sheet1")
         selected_sheet2 = st.selectbox('二つ目のファイルからシートを選択', sheets_file2, key="selected_sheet2")
 
-        # 選択されたシートを読み込み（ヘッダー行を指定）
         df1 = pd.read_excel(file1, sheet_name=selected_sheet1, header=5)
         df2 = pd.read_excel(file2, sheet_name=selected_sheet2, header=0)
 
-        # 2のシートのヘッダー名に対応する1のシートのヘッダーを選択
         st.subheader('2のシートのヘッダー名に対応する1のシートのヘッダーを選択してください')
         header_mappings = {}
         for col in df2.columns:
-            options = list(df1.columns)
+            options = ['転記しない'] + list(df1.columns)
             selected_option = st.selectbox(f'2のシートのヘッダー "{col}" に対応する1のシートのヘッダーを選択:', options, key=col+"_header")
             header_mappings[col] = selected_option
 
-        # 転記実行ボタン
         if st.button('転記実行'):
-            # ここに転記のロジックを追加
             st.write('情報転記を実行中...')
 
-            # 転記処理が完了したら表示
+            # 転記処理
+            for col2, col1 in header_mappings.items():
+                if col1 != '転記しない':
+                    df2[col2] = df1[col1]
+
+            # 転記が完了したシートをCSVとして保存
+            output_csv = df2.to_csv(index=False)
+            st.download_button(label='完成したシートをダウンロード',
+                               data=output_csv,
+                               file_name='completed_sheet.csv',
+                               mime='text/csv')
+
             st.write('情報転記が完了しました！')
 
 if __name__ == "__main__":
